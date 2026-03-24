72dd1a9474c9633fde14e4508ab0d3956c49fdbe
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-25 01:01:28 +0530

    add checkBoxState to manage checkbox status

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,6 @@
 // config
 export { APP_CONFIG } from './config/constants.js';
 export { env } from './config/env.js';
+
+// state
+export { checkBoxState } from './state/checkbox.js';
---
+++ src/core/state/checkbox.ts
@@ -0,0 +1,2 @@
+// create a state to keep track of the status of checkboxes
+export const checkBoxState = new Array<boolean>(100).fill(false);
