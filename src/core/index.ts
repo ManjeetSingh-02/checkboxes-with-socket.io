01380fe3bb32e3c82482f046bd5348e25cfeab0a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 03:04:05 +0530

    add Redis client setup for publisher and subscriber

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,5 +2,8 @@
 export { APP_CONFIG } from './config/constants.js';
 export { env } from './config/env.js';
 
+// redis
+export { redisPublisherClient, redisSubscriberClient } from './redis/client.js';
+
 // state
 export { checkBoxState } from './state/checkbox.js';
---
+++ src/core/redis/client.ts
@@ -0,0 +1,17 @@
+// internal-imports
+import { env } from '../config/env.js';
+
+// external-imports
+import { Redis } from 'ioredis';
+
+// create a new Redis client instance
+export const redisPublisherClient = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+});
+
+// create a new Redis client instance
+export const redisSubscriberClient = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+});
